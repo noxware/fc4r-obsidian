5df735c11c310e5b572eac14fdf966d337f84c56
Noxware <7684329+noxware@users.noreply.github.com>
2023-09-17 02:15:33 -0300

    reverse engineered way to fetch the app url

--- src/localProtocol.ts
+++ src/localProtocol.ts
@@ -1,6 +1,18 @@
-// Internal protocol for local files from Obsidian?
-const prefix = `app://97ff213c3aa1aba2c27af4a1b7fb7f238a24/`;
+// Is something like `app://97ff213c3aa1aba2c27af4a1b7fb7f238a24/`.
+// This internal scheme used by Obsidian can access files outside the vault.
+let prefix: string | null = null;
+
+// Reverse engineered way to fetch the prefix, with help of ChatGPT.
+// The chat export can be found in the `docs` folder.
+function ensurePrefix() {
+	if (prefix === null) {
+		// @ts-ignore
+		const { ipcRenderer } = window.electron;
+		prefix = ipcRenderer.sendSync("file-url");
+	}
+}
 
 export function convertPathToUrl(path: string) {
+	ensurePrefix();
 	return `${prefix}${encodeURIComponent(path)}`;
 }
